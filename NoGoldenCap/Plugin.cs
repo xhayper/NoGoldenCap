e8cf1dba40d3e84edefd9b835b6eb88bb8391048
xhayper <hayper1919@gmail.com>
2022-08-29 19:19:51 +0700

    feat: i forgot to commit

--- NoGoldenCap/Plugin.cs
+++ NoGoldenCap/Plugin.cs
@@ -1,6 +1,37 @@
-﻿namespace a;
+﻿using System.Reflection;
+using HarmonyLib;
+using BepInEx;
 
-public class Plugin
+namespace NoGoldenCap;
+
+[BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+[HarmonyPatch]
+public class Plugin: BaseUnityPlugin
 {
+    private const string PLUGIN_GUID = "io.github.xhayper.NoGoldenCap";
+    private const string PLUGIN_NAME = "NoGoldenCap";
+    private const string PLUGIN_VERSION = "1.0.0";
+
+    private static readonly Harmony Harmony = new(PLUGIN_GUID);
+
+    private void OnEnable()
+    {
+        Harmony.PatchAll(Assembly.GetExecutingAssembly());
+        Logger.LogInfo("Have fun one shotting the boss :)");
+    }
+
+    private void OnDisable()
+    {
+        Harmony.UnpatchSelf();
+    }
     
+    [HarmonyPatch(typeof(PlayerFleeceManager), nameof(PlayerFleeceManager.IncrementDamageModifier))]
+    [HarmonyPrefix]
+    public static bool IncrementDamageModifierPrefix()
+    {
+        if (DataManager.Instance.PlayerFleece != 1) return false;
+        PlayerFleeceManager.damageMultiplier += 0.1f;
+        PlayerFleeceManager.OnDamageMultiplierModified?.Invoke(PlayerFleeceManager.damageMultiplier);
+        return false;
+    }
 }
